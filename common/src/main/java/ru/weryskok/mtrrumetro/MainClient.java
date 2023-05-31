65ab2f6b2e228a886e9de90f7f7be1c94eee9877
Alexander Minkin <weryskok@gmail.com>
2023-05-31 17:22:57 +0300

    Remove glass components from ticket machine and use cutoutMipped render Type Had some rendering issues with it

--- common/src/main/java/ru/weryskok/mtrrumetro/MainClient.java
+++ common/src/main/java/ru/weryskok/mtrrumetro/MainClient.java
@@ -8,15 +8,15 @@ import net.minecraft.client.renderer.RenderType;
 
 public class MainClient {
     public static void init() {
-        RegistryClient.registerBlockRenderType(RenderType.cutout(), Blocks.MOSCOW_OLD_TICKET_BARRIER_ENTRANCE.get());
-        RegistryClient.registerBlockRenderType(RenderType.cutout(), Blocks.MOSCOW_OLD_TICKET_BARRIER_EXIT.get());
-        RegistryClient.registerBlockRenderType(RenderType.cutout(), Blocks.MOSCOW_OLD_TICKET_BARRIER_SIDE_COVER.get());
+        RegistryClient.registerBlockRenderType(RenderType.cutoutMipped(), Blocks.MOSCOW_OLD_TICKET_BARRIER_ENTRANCE.get());
+        RegistryClient.registerBlockRenderType(RenderType.cutoutMipped(), Blocks.MOSCOW_OLD_TICKET_BARRIER_EXIT.get());
+        RegistryClient.registerBlockRenderType(RenderType.cutoutMipped(), Blocks.MOSCOW_OLD_TICKET_BARRIER_SIDE_COVER.get());
 
         RegistryClient.registerBlockRenderType(RenderType.translucent(), Blocks.MOSCOW_NEW_TICKET_BARRIER_ENTRANCE.get());
         RegistryClient.registerBlockRenderType(RenderType.translucent(), Blocks.MOSCOW_NEW_TICKET_BARRIER_EXIT.get());
         RegistryClient.registerBlockRenderType(RenderType.translucent(), Blocks.MOSCOW_NEW_TICKET_BARRIER_SIDE_COVER.get());
 
-        RegistryClient.registerBlockRenderType(RenderType.translucent(), Blocks.MOSCOW_NEW_TICKET_MACHINE.get());
+        RegistryClient.registerBlockRenderType(RenderType.cutoutMipped(), Blocks.MOSCOW_NEW_TICKET_MACHINE.get());
 //        Config.refreshProperties();
     }
 
